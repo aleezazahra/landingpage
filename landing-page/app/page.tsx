57fe18f7ea24ab12e13a750e6025cf63500ae591
aleezazahra <azahradum@gmail.com>
2026-06-30 22:55:07 +0500

    made globals.css

---
+++ landing-page/app/page.tsx
@@ -0,0 +1,11 @@
+
+"use client";
+import React from 'react'
+import Hero from "@/components/Hero"
+
+const Hero=()=>{
+     return <div className="h-full overflow-x-hidden">
+    <Hero />
+
+  </div>
+}
